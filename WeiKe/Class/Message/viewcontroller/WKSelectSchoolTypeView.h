8a8c1e90f8b70d7539e4f328f084ad6beaa2f920
华驰科技 <1061401072@qq.com>
2017-06-22 18:20:49 +0800

    项目完善

---
+++ WeiKe/Class/Message/viewcontroller/WKSelectSchoolTypeView.h
@@ -0,0 +1,13 @@
+//
+//  WKSelectSchoolTypeView.h
+//  WeiKe
+//
+//  Created by 华驰科技 on 2017/6/22.
+//  Copyright © 2017年 谢鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WKSelectSchoolTypeView : UIView
+
+@end
---
+++ WeiKe/Class/Message/viewcontroller/WKSelectSchoolTypeView.m
@@ -0,0 +1,21 @@
+//
+//  WKSelectSchoolTypeView.m
+//  WeiKe
+//
+//  Created by 华驰科技 on 2017/6/22.
+//  Copyright © 2017年 谢鹏. All rights reserved.
+//
+
+#import "WKSelectSchoolTypeView.h"
+
+@implementation WKSelectSchoolTypeView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
